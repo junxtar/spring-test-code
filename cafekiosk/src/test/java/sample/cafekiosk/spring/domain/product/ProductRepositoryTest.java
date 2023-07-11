cf9bb473da74d763f892a7c67f5032e848de2dbb
junxtar <aa1870088@naver.com>
2023-07-11 16:28:02 +0900

    created order test code

--- cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -1,18 +1,15 @@
 package sample.cafekiosk.spring.domain.product;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.HOLD;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.STOP_SELLING;
