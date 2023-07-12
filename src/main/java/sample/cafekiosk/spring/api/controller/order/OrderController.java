6e750d87d81f0ac129da0a0e9dff67c29ca76ade
junxtar <aa1870088@naver.com>
2023-07-12 17:14:33 +0900

    feat: implemented order controller

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.service.order.OrderService;
-import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,9 +16,10 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/api/v1/order/new")
-    public void createOrder(@RequestBody OrderCreateRequest request) {
-        orderService.createOrder(request);
+    @PostMapping("/api/v1/orders/new")
+    public OrderResponse createOrder(@RequestBody OrderCreateRequest request) {
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        return orderService.createOrder(request, registeredDateTime);
 
     }
 }
