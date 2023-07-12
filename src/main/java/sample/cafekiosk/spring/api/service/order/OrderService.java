7a808da7ac34f6004dc827328de71e157ba3ea46
junxtar <aa1870088@naver.com>
2023-07-12 17:15:47 +0900

    feat: implemented create order service

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -1,13 +1,45 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.orderproduct.OrderRepository;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class OrderService {
 
-    public OrderResponse createOrder(OrderCreateRequest request) {
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
+        List<String> productNumbers = request.getProductNumbers();
+        // product
+        List<Product> duplicateProducts = findProductBy(productNumbers);
 
+        //order
+        Order order = Order.create(duplicateProducts, registeredDateTime);
+        Order savedOrder = orderRepository.save(order);
+
+        return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
     }
 }
