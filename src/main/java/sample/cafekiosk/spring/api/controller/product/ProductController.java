45f59743145004a5d4b003e2888abd20f008cfb3
junxtar <aa1870088@naver.com>
2023-07-12 17:16:14 +0900

    feat: implemented create product controller

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,13 +1,10 @@
 package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
-import sample.cafekiosk.spring.domain.product.ProductRepository;
-
 import java.util.List;
 
 @RequiredArgsConstructor
