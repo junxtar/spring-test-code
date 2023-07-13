5a294b42a288727ccbee7b22ca49e512992ab2c4
junxtar <aa1870088@naver.com>
2023-07-13 19:09:43 +0900

    implemented stock entity

---
+++ src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
@@ -0,0 +1,50 @@
+package sample.cafekiosk.spring.domain.stock;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.global.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Stock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    private int quantity;
+
+    @Builder
+    private Stock(String productNumber, int quantity) {
+        this.productNumber = productNumber;
+        this.quantity = quantity;
+    }
+
+    public static Stock create(String productNumber, int quantity) {
+        return Stock.builder()
+                .productNumber(productNumber)
+                .quantity(quantity)
+                .build();
+    }
+
+    public boolean isQuantityLessThan(int quantity) {
+        return this.quantity < quantity;
+    }
+
+    public void reduceQuantity(int quantity) {
+        if (this.quantity < quantity) {
+            throw new IllegalArgumentException("차감할 재고가 없습니다.");
+        }
+        this.quantity -= quantity;
+    }
+}
