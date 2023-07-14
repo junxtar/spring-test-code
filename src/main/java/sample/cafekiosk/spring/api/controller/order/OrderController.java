392b5cfa68cc035216d557e12ff1fa4997da5c5b
junxtar <aa1870088@naver.com>
2023-07-14 11:41:28 +0900

    implemented order controller

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -4,22 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.ApiResponse;
+import sample.cafekiosk.spring.api.controller.order.dto.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.OrderService;
-import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
-import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.api.service.order.dto.response.OrderResponse;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
 public class OrderController {
 
-    private final OrderService orderService;
+   private final OrderService orderService;
+   @PostMapping("/api/v1/orders/new")
+   public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
+      LocalDateTime registeredDateTime = LocalDateTime.now();
+      return ApiResponse.ok(orderService.createOrder(request.toServiceRequest(), registeredDateTime));
+   }
 
-    @PostMapping("/api/v1/orders/new")
-    public OrderResponse createOrder(@RequestBody OrderCreateRequest request) {
-        LocalDateTime registeredDateTime = LocalDateTime.now();
-        return orderService.createOrder(request, registeredDateTime);
-
-    }
 }
