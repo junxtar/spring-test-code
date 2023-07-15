7bb7ab40887c525dcea537a062858124e6e6f656
junxtar <aa1870088@naver.com>
2023-07-15 15:56:49 +0900

    feat: implemented order service dto

--- src/main/java/sample/cafekiosk/spring/api/service/order/dto/request/OrderCreateServiceRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/dto/request/OrderCreateServiceRequest.java
@@ -15,7 +15,7 @@ public class OrderCreateServiceRequest {
    private List<String> productNumbers;
 
    @Builder
-   public OrderCreateServiceRequest(List<String> productNumbers) {
+   private OrderCreateServiceRequest(List<String> productNumbers) {
       this.productNumbers = productNumbers;
    }
 }
