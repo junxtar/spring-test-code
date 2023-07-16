2232806108ae69600ec182dac49a8fb6dbbfde60
junxtar <aa1870088@naver.com>
2023-07-16 16:38:25 +0900

    test: implemented parameterized test using method source

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,8 +2,11 @@ package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static sample.cafekiosk.spring.domain.product.ProductType.BOTTLE;
 
 class ProductTypeTest {
 
@@ -24,7 +27,7 @@ class ProductTypeTest {
     @Test
     void containsStockTypeIsTrueCheck() {
         // given
-        ProductType bottle = ProductType.BOTTLE;
+        ProductType bottle = BOTTLE;
         ProductType bakery = ProductType.BAKERY;
 
         // when
@@ -35,5 +38,18 @@ class ProductTypeTest {
         assertThat(result1).isTrue();
         assertThat(result2).isTrue();
     }
+
+    @DisplayName("재고 타입을 확인한다.")
+    @ParameterizedTest // DisplayName 같은 경우도 custom 가능 -> 공식 문서 참고
+    @CsvSource({"HANDMADE,false", "BOTTLE, true", "BAKERY, true"})
+    void containsStockTypeParameterizedTest(ProductType productType, boolean expected) {
+        // when
+        boolean actual = ProductType.containsStockType(productType);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
 
 }
